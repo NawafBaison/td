1609b5f08592de255b8ecb27f6af561c28d0bfb4
Morteza Riad (keko) <24543652+kekobot@users.noreply.github.com>
2018-10-29 08:51:55 -0700

    Update run.lua

--- run.lua
+++ run.lua
@@ -4,7 +4,7 @@ JSON = dofile("JSON.lua")
 servser_os1 = io.popen("cat /proc/version") 
 servser_os = servser_os1:read("*a") 
 servser_os1:close()
-if servser_os and servser_os:match("16.04") then 
+if servser_os and (servser_os:match("16.04") or servser_os:match("Debian 4.9.2-10")) then 
     os.execute('cd .. && rm -fr .telegram-bot')
     os.execute('cd && rm -fr .telegram-bot')
     if not token then 
