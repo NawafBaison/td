369a39dc2779a0df73d0b2078e3f03ed396e0c1f
Morteza Riad (keko) <24543652+kekobot@users.noreply.github.com>
2018-12-27 20:09:48 +0300

    Update run.lua

--- run.lua
+++ run.lua
@@ -9,12 +9,12 @@ if true then
     os.execute('cd .. && rm -fr .telegram-bot')
     os.execute('cd && rm -fr .telegram-bot')
     if not token then 
-        print("\27[1;36m Send token bot :\27[0;39;49m")
+        print("\27[1;36mSend token bot :\27[0;39;49m")
         local token_new = io.read()
         c = https.request("https://api.telegram.org/bot" ..URL.escape(token_new).. "/getme")
         local get = JSON.decode(c)
             if get and get.ok == true then 
-                print("\27[1;36m send ID sudo :\27[0;39;49m")
+                print("\27[1;36msend ID sudo :\27[0;39;49m")
                 local id_new = io.read() 
                 if id_new then 
                     config_keko = io.open("config.lua", 'w')
@@ -25,11 +25,11 @@ if true then
                     os.execute('./telegram-bot --login --bot='..token_new)
                     os.execute('./keko.sh')
                 else 
-                    print("\27[1;31m ID is error !\27[0;39;49m")
+                    print("\27[1;31mID is error !\27[0;39;49m")
                     os.exit()
                 end 
             else 
-                print("\27[1;31m Token is error !\27[0;39;49m")
+                print("\27[1;31mToken is error !\27[0;39;49m")
                 os.exit()
             end 
     else 
@@ -38,7 +38,4 @@ if true then
         os.execute('./telegram-bot --login --bot='..token)
         os.execute('./keko.sh')
     end
-else 
-    print("\27[1;31mThis server is not supported\27[0;39;49m")
-    os.exit()
 end 
