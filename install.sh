c236349f3649c3b64b1f572bf35ee583a310c8df
Morteza Riad (keko) <24543652+kekobot@users.noreply.github.com>
2018-12-27 20:04:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd td 
-sudo apt-get -f install -y
+sudo apt-get -f install 
 echo "wit ...."
 sudo apt-get update -y 
 clear
@@ -17,52 +17,83 @@ echo '│ #   "m "#mm"  #  "m  "#m#"         "mmm"  "#m#"  "mm"#   #     "#mm"  
 echo "│                                                                                   │"
 echo "│                        Channel : t.me/botlua                                      │"
 echo "└─────────────────────────────────────────────────────────────────────────────┘"
-sudo apt-get install git screen redis-server shc lua5.2 liblua5.2-dev lua-lgi libnotify-dev unzip tmux -y
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
 
+sudo apt-get install git screen redis-server shc lua5.2 liblua5.2-dev lua-lgi libnotify-dev unzip tmux 
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson* libpython-dev make unzip git redis-server g++ -y --force-yes
+
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+
+tar zxpf luarocks-2.2.2.tar.gz
+
+cd luarocks-2.2.2
+
+./configure; sudo make bootstrap
+
+sudo luarocks install luasocket
+
+sudo luarocks install luasec
+
+sudo luarocks install redis-lua
+
+sudo luarocks install ansicolors
+
+sudo luarocks install serpent
+
+cd ..
+
+sudo apt-get install curl -y
+
+rm -fr luarocks-2.2.2.tar.gz
+
+rm -fr install.sh
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test  
 
+sudo apt-get update 
 
-sudo apt-get update -y
-apt-get upgrade -y
-sudo apt-get install libconfig++9v5 libstdc++6 -y
-sudo apt autoremove -y
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz 
-tar zxpf luarocks-2.2.2.tar.gz
-rm luarocks-2.2.2.tar.gz
-cd luarocks-2.2.2
-./configure
-sudo make bootstrap
-cd ..
-rm -rf luarocks-2*
+apt-get upgrade 
+
+sudo apt-get install libconfig++9v5 libstdc++6 
+
+sudo apt autoremove 
+
 wget https://valtman.name/files/telegram-bot-170904-nightly-linux
+
 mv telegram-bot-170904-nightly-linux telegram-bot
+
 chmod +x telegram-bot
+
 rm -fr telegram-bot-170904-nightly-linux
-sudo luarocks install luasocket
-sudo luarocks install luasec
-sudo luarocks install redis-lua
-sudo luarocks install lua-term
-sudo luarocks install serpent
-sudo luarocks install dkjson
-sudo luarocks install lanes
-sudo luarocks install Lua-cURL
+
 sudo service redis-server start
+
 wget http://botlua.tk/keko.sh
+
 wget http://botlua.tk/fix.sh
+
 sudo chmod +x fix.sh
+
 sudo chmod go-r fix.sh
+
 sudo chmod +x keko.sh
+
 sudo chmod 777 keko.sh
+
 sudo chmod 777 fix.sh
+
 sudo chmod go-r keko.sh
+
 sudo chmod 777 run
+
 sudo chmod 777 runAU
 
-sudo ./fix.sh 
+echo "Wit plz ....."
+
+./fix.sh 
 
 clear
+
 echo " ──────────────────────────────────────────────────────────────────────────────"
 echo "            Devloper : t.me/HHHHD | My Channel : t.me/botlua                   "
 echo "                                                                               "
